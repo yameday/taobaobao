fdc6c6910a4cc6a9b4307df81245c59659a01b09
KayCheng <kaycheng717@gmail.com>
2018-03-22 19:43:28 +0800

    Create reply model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321041319) do
+ActiveRecord::Schema.define(version: 20180322075515) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
