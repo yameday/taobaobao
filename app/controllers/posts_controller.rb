23d9af9e6bfe63167de617f92bdb1052928dcf96
francishuang <francis3925@gmail.com>
2018-03-23 05:11:35 +0800

    create posts#index, new, create, edit, update and basic posts/index view page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,44 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:edit, :update]
+  
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to root_path
+      # temporary use
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :photo, :url, :description, :delivery, :price, :goal, :due_time)
+  end
+
 end
