53af7a8eacdf6489fd1fcdc9cfed80aea8417281
KayCheng <kaycheng717@gmail.com>
2018-03-28 20:29:45 +0800

    Root page didn't meed login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :find_post, only: [:show, :edit, :update]
   
   def index
