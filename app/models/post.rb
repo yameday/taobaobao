d22942e473eac765f6f6d83bd0fb8bf615e93128
francishuang <francis3925@gmail.com>
2018-03-31 02:50:35 +0800

    add status label on posts/index partials and posts/show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,14 @@ class Post < ApplicationRecord
     self.participated_user.include?(user)
   end
 
+  def opening?
+    !over_due?
+  end
+
+  private
+
+  def over_due?
+    Time.now > due_time
+  end
+
 end
