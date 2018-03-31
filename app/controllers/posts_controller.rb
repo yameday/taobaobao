143228c77817236780d039cf1f3a3087d4c4af79
KayCheng <kaycheng717@gmail.com>
2018-03-31 18:14:00 +0800

    Change the post show page style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
-      redirect_to root_path
+      redirect_to posts_path
     else
       render :new
     end
