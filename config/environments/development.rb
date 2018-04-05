f704cadb90e5f97d0946bbe85e48a4cb0c280536
KayCheng <kaycheng717@gmail.com>
2018-04-05 23:56:05 +0800

    Create user mailer for participate action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   config.action_mailer.perform_caching = false
 
