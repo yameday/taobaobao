190bc56a6e36c74d6a36399b1a7deeb97db50eda
KayCheng <kaycheng717@gmail.com>
2018-04-06 20:12:16 +0800

    Fix the user_mailer.rb

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: "Taobaobao <taobaobao99@gmail.com>"
+
+  def notify_participate_create(participate)
+    
+    @participate = participate
+
+    mail to: participate.user.email,
+    subject: "Taobaobao | 已參加: #{participate.post.title} 團購"
+  end
+end
