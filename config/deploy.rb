0ea1873471479e92a21f6517de29bdf7d1ab3d70
sakuraki <a1989198@hotmail.com>
2018-04-17 22:05:00 +0800

    Auto stash before merge of "develop" and "origin/develop"
    link uploads dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/deploy/taobaobao'
  append :linked_files, "config/database.yml", "config/secrets.yml", "config/facebook.yml", "config/email.yml"
 
 # Default value for linked_dirs is []
- append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
